4fa30441c41a4066d70f24dc3ff5108f13162e61
matheustorresii <matheustf2003@gmail.com>
2024-07-06 01:52:41 -0300

    Initial Commit

---
+++ DylibContainer/DylibContainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DylibContainerApp.swift
+//  DylibContainer
+//
+//  Created by Matheus Torres on 06/07/24.
+//
+
+import SwiftUI
+
+@main
+struct DylibContainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
